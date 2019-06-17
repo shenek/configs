3e467429382d0aced218cd1d93fee839731b985b
Stepan Henek <stepan@henek.name>
2019-06-17 11:00:35 +0200

    new_shell.sh: improved cwd detection

--- config/i3/scripts/new_shell.sh
+++ config/i3/scripts/new_shell.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
-PID=$(($(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3) + 2))
-if [ -e "/proc/$PID/cwd" ]
+PARENT_PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
+BASH_PID=$(pgrep -P $PARENT_PID bash)
+if [ -e "/proc/$BASH_PID/cwd" ]
 then
-    TERMINAL=urxvt i3-sensible-terminal -cd "$(readlink /proc/$PID/cwd)"
+    TERMINAL=urxvt i3-sensible-terminal -cd "$(readlink /proc/$BASH_PID/cwd)"
 else
     TERMINAL=urxvt i3-sensible-terminal
 fi
