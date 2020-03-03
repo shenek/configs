873e5aa760ef8d4c60b3621c71fa940c85bc38e6
Stepan Henek <stepan@angelcam.com>
2020-03-03 16:06:22 +0100

    use alacritty as a default terminal

--- config/i3/scripts/new_shell.sh
+++ config/i3/scripts/new_shell.sh
@@ -4,7 +4,7 @@ PARENT_PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 BASH_PID=$(pgrep -P $PARENT_PID bash)
 if [ -e "/proc/$BASH_PID/cwd" ]
 then
-    TERMINAL=urxvt i3-sensible-terminal -cd "$(readlink /proc/$BASH_PID/cwd)"
+    alacritty --working-directory "$(readlink /proc/$BASH_PID/cwd)"
 else
-    TERMINAL=urxvt i3-sensible-terminal
+    alacritty
 fi
