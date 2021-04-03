efd632a23424c3324be4005fa75df8080cfca2db
Stepan Henek <stepan@henek.name>
2021-04-03 21:47:21 +0200

    gitconfig: fix versiontag for README.rst

--- scripts/git_version_tag.sh
+++ scripts/git_version_tag.sh
@@ -3,7 +3,7 @@ LAST_VERSION="$(git describe --tags --abbrev=0 | sed -e 's/^v//' -e 's/^package-
 
 # Try to reach CHANGELOG.rst
 if [ -f CHANGELOG.rst ]; then
-	cat CHANGELOG.rst 2>/dev/null | awk "/^'${LAST_VERSION}' / { exit } { print $0 }"
+	cat CHANGELOG.rst 2>/dev/null | awk "/^${LAST_VERSION} / { exit } { print \$0 }"
 	exit 0
 fi
 
